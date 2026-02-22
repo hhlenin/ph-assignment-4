4639cd3ee8ed64138bfcbd90013052f497d30785
L from RW <hasibul.lenin@gmail.com>
2026-02-22 11:17:32 -0500

    grabbing childs to pass on different tabs

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const accordion = document.getElementById('accordion');
+const accordion = document.getElementById('accordion-all');
 let totalJobs = accordion.children.length;
 document.getElementById("total-jobs").innerText = totalJobs;
 document.getElementById("card-total-jobs").innerText = totalJobs;
@@ -7,30 +7,30 @@ getCount();
 
 
 
-accordion.addEventListener('click', function (e) {
-    const children = this.children;
-    for (const child of children) {
-        if (child.classList.contains("interviewed")) {
-            interviewJobs++;
-        }
-        if (child.classList.contains("rejected")) {
-            rejectedJobs++;
-        }
+// accordion.addEventListener('click', function (e) {
+//     const children = this.children;
+//     for (const child of children) {
+//         if (child.classList.contains("interviewed")) {
+//             interviewJobs++;
+//         }
+//         if (child.classList.contains("rejected")) {
+//             rejectedJobs++;
+//         }
 
-    }
-    console.log(interviewJobs, rejectedJobs)
+//     }
+//     console.log(interviewJobs, rejectedJobs)
 
-    // for(let i= 0; i< childs.length; i++) {
-    //     console.log(childs[i])
-    //     (childs[i].hasAttribute("open"))
-    // }
-    // if (e.target.tagName === "SUMMARY") {
-    //     e.target.parentNode.classList.remove("border-base-300");
-    //     e.target.parentNode.classList.add("border-white");
-    //     e.stopPropagation();
-    // }
+// for(let i= 0; i< childs.length; i++) {
+//     console.log(childs[i])
+//     (childs[i].hasAttribute("open"))
+// }
+// if (e.target.tagName === "SUMMARY") {
+//     e.target.parentNode.classList.remove("border-base-300");
+//     e.target.parentNode.classList.add("border-white");
+//     e.stopPropagation();
+// }
 
-});
+// });
 
 function getCount() {
     let rejectedJobs = 0;
@@ -52,17 +52,37 @@ function getCount() {
     document.getElementById("card-interviewed-jobs").innerText = interviewJobs;
 }
 
-document.getElementById("rejected-tab").addEventListener('click', function(){
+document.getElementById("rejected-tab").addEventListener('click', function () {
     document.getElementById("heading").innerText = "Rejected";
-    document.getElementById("total-jobs").innerText =document.getElementById("card-rejected-jobs").innerText;
+    document.getElementById("total-jobs").innerText = document.getElementById("card-rejected-jobs").innerText;
 });
 
-document.getElementById("all-tab").addEventListener('click', function(){
+document.getElementById("all-tab").addEventListener('click', function () {
     document.getElementById("heading").innerText = "Available";
     document.getElementById("total-jobs").innerText = document.getElementById("card-total-jobs").innerText;
 });
-document.getElementById("interviewed-tab").addEventListener('click', function(){
+document.getElementById("interviewed-tab").addEventListener('click', function () {
     document.getElementById("heading").innerText = "Interviewed";
-    document.getElementById("total-jobs").innerText =document.getElementById("card-interviewed-jobs").innerText;
+    document.getElementById("total-jobs").innerText = document.getElementById("card-interviewed-jobs").innerText;
+});
+
+accordion.addEventListener("click", function () {
+    console.log('ddssgd');
+    
+    // document.getElementById("accordion-").
+    for (const child of this.children) {
+        if (child.classList.contains("interviewed")) {
+            console.log(child);
+            
+            // document.getElementById("accordion-").appendChild(child)
+            document.getElementById("accordion-").innerHTML('bello')
+
+        }
+        if (child.classList.contains("rejected")) {
+            document.getElementById("accordion").appendChild(child)
+
+        }
+
+    }
 });
 
