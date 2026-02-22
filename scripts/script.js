1ae6e8183627a82d54ea0bc5a2e57a685f18aef5
L from RW <hasibul.lenin@gmail.com>
2026-02-22 09:12:03 -0500

    couting set done

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,68 @@
 const accordion = document.getElementById('accordion');
-accordion.addEventListener('click', function(e){
-    const childs = this.childNodes;
-    // console.log(childs);
-    
-    for(let i= 0; i< childs.length; i++) {
-        console.log(childs[i])
-        (childs[i].hasAttribute("open"))
+let totalJobs = accordion.children.length;
+document.getElementById("total-jobs").innerText = totalJobs;
+document.getElementById("card-total-jobs").innerText = totalJobs;
+
+getCount();
+
+
+
+accordion.addEventListener('click', function (e) {
+    const children = this.children;
+    for (const child of children) {
+        if (child.classList.contains("interviewed")) {
+            interviewJobs++;
+        }
+        if (child.classList.contains("rejected")) {
+            rejectedJobs++;
+        }
+
     }
-    if (e.target.tagName === "SUMMARY") {
-        e.target.parentNode.classList.remove("border-base-300");
-        e.target.parentNode.classList.add("border-white");
-        e.stopPropagation();
-    }
-    
+    console.log(interviewJobs, rejectedJobs)
+
+    // for(let i= 0; i< childs.length; i++) {
+    //     console.log(childs[i])
+    //     (childs[i].hasAttribute("open"))
+    // }
+    // if (e.target.tagName === "SUMMARY") {
+    //     e.target.parentNode.classList.remove("border-base-300");
+    //     e.target.parentNode.classList.add("border-white");
+    //     e.stopPropagation();
+    // }
+
 });
+
+function getCount() {
+    let rejectedJobs = 0;
+    let interviewJobs = 0;
+
+    for (const child of accordion.children) {
+        if (child.classList.contains("interviewed")) {
+            interviewJobs++;
+        }
+        if (child.classList.contains("rejected")) {
+            rejectedJobs++;
+        }
+
+    }
+    // document.getElementById("total-jobs").innerText = totalJobs;
+    document.getElementById("card-rejected-jobs").innerText = rejectedJobs;
+
+    // document.getElementById("total-jobs").innerText = totalJobs;
+    document.getElementById("card-interviewed-jobs").innerText = interviewJobs;
+}
+
+document.getElementById("rejected-tab").addEventListener('click', function(){
+    document.getElementById("heading").innerText = "Rejected";
+    document.getElementById("total-jobs").innerText =document.getElementById("card-rejected-jobs").innerText;
+});
+
+document.getElementById("all-tab").addEventListener('click', function(){
+    document.getElementById("heading").innerText = "Available";
+    document.getElementById("total-jobs").innerText = document.getElementById("card-total-jobs").innerText;
+});
+document.getElementById("interviewed-tab").addEventListener('click', function(){
+    document.getElementById("heading").innerText = "Interviewed";
+    document.getElementById("total-jobs").innerText =document.getElementById("card-interviewed-jobs").innerText;
+});
+
